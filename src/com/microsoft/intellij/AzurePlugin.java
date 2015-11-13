e9e03ab8a23f7124cfb5a1f4b05c58b1b22ef3c0
lenala <lena.lakhno@gmail.com>
2015-11-13 12:19:24 -0800

    update to azure sdk 0.9.0

--- src/com/microsoft/intellij/AzurePlugin.java
+++ src/com/microsoft/intellij/AzurePlugin.java
@@ -71,7 +71,7 @@ public class AzurePlugin extends AbstractProjectComponent {
     public static final String COMMON_LIB_PLUGIN_ID = "azure-services-explorer-plugin";
     public static final String COMPONENTSETS_VERSION = "2.8.0"; // todo: temporary fix!
     private static final String PREFERENCESETS_VERSION = "2.8.0";
-    public static final String AZURE_LIBRARIES_VERSION = "0.7.0";
+    public static final String AZURE_LIBRARIES_VERSION = "0.9.0";
     public static final String QPID_LIBRARIES_VERSION = "0.19.0";
     public final static int REST_SERVICE_MAX_RETRY_COUNT = 7;
 
